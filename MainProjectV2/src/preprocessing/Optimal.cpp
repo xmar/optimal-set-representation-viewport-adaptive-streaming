401c81826e9e0d8d954c160bbcc2bf33c4c8e75b
Xavier Corbillon <xavier.corbillon@telecom-bretagne.eu>
2017-03-30 09:48:50 +0200

    A new test

--- MainProjectV2/src/preprocessing/Optimal.cpp
+++ MainProjectV2/src/preprocessing/Optimal.cpp
@@ -175,20 +175,16 @@ void Optimal::Run(void)
   std::cout << "Nb views = " << psi_vid->NbView() << std::endl;
 
   //Create variables
-  IloNumVarArray b_qer(env, nbUser, 0, max_b_qer, ILOFLOAT); //b_qer[u]
-  IloNumVarArray b_out(env, nbUser, 0, max_b_out, ILOFLOAT); //b_out[u]
+  VarMatrix q_b_qer(env, nbUser); //q_b_qer[u][a]
+  VarMatrix q_b_out(env, nbUser); //q_b_out[u][a]
   for (unsigned u = 0; u  < nbUser; ++u)
   {
-    b_qer[u].setName((std::string("b_qer[") +std::to_string(u)+ std::string("]")).c_str());
-    b_out[u].setName((std::string("b_out[") +std::to_string(u)+ std::string("]")).c_str());
+    q_b_qer[u] = IloNumVarArray(env, nbArea, 0, max_b_qer, ILOFLOAT);
+    q_b_out[u] = IloNumVarArray(env, nbArea, 0, max_b_qer, ILOFLOAT);
   }
   IloNumVarArray b_qer_j(env, m_conf->nbQer, min_b_qer, max_b_qer, ILOFLOAT); //b_qer[u]
   IloNumVarArray b_out_j(env, m_conf->nbQer, min_b_out, max_b_out, ILOFLOAT); //b_out[u]
-  for (unsigned j = 0; j < m_conf->nbQer; ++j)
-  {
-    b_qer_j[j].setName((std::string("b_qer_j[") +std::to_string(j)+ std::string("]")).c_str());
-    b_qer_j[j].setName((std::string("b_qer_j[") +std::to_string(j)+ std::string("]")).c_str());
-  }
+
 
   VarMatrix q_qer(env, nbUser); // q_qer[u][a]
   for (unsigned u = 0; u < nbUser; ++u)
@@ -197,6 +193,27 @@ void Optimal::Run(void)
     for (unsigned a = 0; a < nbArea; ++a)
     {
       q_qer[u][a].setName((std::string("q_qer[") +std::to_string(u)+ std::string("][") +std::to_string(a)+ std::string("]")).c_str());
+    }
+  }
+  VarMatrix s(env, nbUser); // q_qer[u][a]
+  VarMatrix d(env, nbUser);
+  for (unsigned u = 0; u < nbUser; ++u)
+  {
+    s[u] = IloNumVarArray(env, m_conf->nbQer, 0, 1, ILOINT);
+    d[u] = IloNumVarArray(env, m_conf->nbQer, 0, 1, ILOINT);
+    for (unsigned j = 0; j < m_conf->nbQer; ++j)
+    {
+      s[u][j].setName((std::string("s[") +std::to_string(u)+ std::string("][") +std::to_string(j)+ std::string("]")).c_str());
+    }
+  }
+
+  VarMatrix nb(env, m_conf->nbQer);
+  for (unsigned j = 0; j < m_conf->nbQer; ++j)
+  {
+    nb[j] = IloNumVarArray(env, nbArea+1, 0, 1, ILOINT);
+    for (unsigned n = 0; n <= nbArea; ++n)
+    {
+      nb[j][n].setName((std::string("nb[") +std::to_string(j)+ std::string("][") +std::to_string(n)+ std::string("]")).c_str());
     }
   }
 
@@ -212,74 +229,93 @@ void Optimal::Run(void)
 
 
   std::cout << "Generate constraints" << std::endl;
-  try
-  {
-  //1.a
-  for (unsigned j = 0; j < m_conf->nbQer; ++j)
-  {
-    for (unsigned n = 0; n <= nbArea; ++n)
-    {
-      IloIfThen selectBitrate(env, IloSum(created_r[j]) == n, (b_qer_j[j] == b_qer_val[n]) && (b_out_j[j] == b_out_val[n]) );
-      selectBitrate.setName((std::string("1a_")+std::to_string(j)+std::string("_")+std::to_string(n)).c_str());
-      model.add(selectBitrate);
-    }
-  }
+  try{
+  // for (unsigned j = 0; j < m_conf->nbQer; ++j)
+  // {
+  //   for (unsigned n = 0; n <= nbArea; ++n)
+  //   {
+  //     IloIfThen selectBitrate(env, IloSum(created_r[j]) == n, (b_qer_j[j] == b_qer_val[n]) && (b_out_j[j] == b_out_val[n]) );
+  //     selectBitrate.setName((std::string("1a_")+std::to_string(j)+std::string("_")+std::to_string(n)).c_str());
+  //     model.add(selectBitrate);
+  //   }
+  // }
 
-  //set tile constraints
-  unsigned nbHTile = 4;
-  unsigned nbVTile = 4;
-  for (unsigned thetaTile = 0; thetaTile < nbHTile; ++thetaTile)
-  {
-    for (unsigned phiTile = 0; phiTile < nbVTile; ++phiTile)
-    {
-      auto areaIds = m_areaSet->GetAreaIdInTile(thetaTile*2*PI/nbHTile - PI, (thetaTile+1)*2*PI/nbHTile - PI,
-                                                phiTile*PI/nbVTile - PI, (phiTile+1)*PI/nbVTile);
-      if (areaIds.size() > 1)
-      {
-        for (unsigned j = 0; j < m_conf->nbQer; ++j)
-        {
-          IloAnd tileContraint(env);
-          for (unsigned i = 1; i < areaIds.size(); ++i)
-          {
-            tileContraint.add(created_r[j][areaIds[0]] == created_r[j][areaIds[i]]);
-          }
-          tileContraint.setName((std::string("tile")+std::to_string(thetaTile)+std::string("_")+std::to_string(phiTile)).c_str());
-          model.add(tileContraint);
-        }
-      }
-    }
-  }
+  // //set tile constraints
+  // unsigned nbHTile = 4;
+  // unsigned nbVTile = 4;
+  // for (unsigned thetaTile = 0; thetaTile < nbHTile; ++thetaTile)
+  // {
+  //   for (unsigned phiTile = 0; phiTile < nbVTile; ++phiTile)
+  //   {
+  //     auto areaIds = m_areaSet->GetAreaIdInTile(thetaTile*2*PI/nbHTile - PI, (thetaTile+1)*2*PI/nbHTile - PI,
+  //                                               phiTile*PI/nbVTile - PI, (phiTile+1)*PI/nbVTile);
+  //     if (areaIds.size() > 1)
+  //     {
+  //       for (unsigned j = 0; j < m_conf->nbQer; ++j)
+  //       {
+  //         IloAnd tileContraint(env);
+  //         for (unsigned i = 1; i < areaIds.size(); ++i)
+  //         {
+  //           tileContraint.add(created_r[j][areaIds[0]] == created_r[j][areaIds[i]]);
+  //         }
+  //         tileContraint.setName((std::string("tile")+std::to_string(thetaTile)+std::string("_")+std::to_string(phiTile)).c_str());
+  //         model.add(tileContraint);
+  //       }
+  //     }
+  //   }
+  // }
+
+  double M = 4*PI;
+  double e = 0.0000001;
 
   for (unsigned u = 0; u < nbUser; ++u)
   {
-    // for (unsigned a = 0; a < nbArea; ++a)
-    // {
-    //   sc += q_qer[u][a] * b_qer[u] + (1-q_qer[u][a]) * b_out[u];
-    // }
-    // //1.b c d e
-    // IloOr or_bitrate(env);
-    // for (unsigned n = 0; n <= nbArea; ++n)
-    // {
-    //   or_bitrate.add((b_qer[u] == b_qer_val[n]) && (b_out[u] == b_out_val[n]));
-    // }
-    // or_bitrate.setName((std::string("1bcde")).c_str());
-    // model.add(or_bitrate);
-
-    //1.f
-    IloOr or_repr(env);
-    for (unsigned j = 0; j < m_conf->nbQer; ++j)
+    //Set the value of q_b_qer[u][a] to the value of q_qer of the selected j, idem for q_b_out[u][a]
+    IloAnd andSec(env);
+    for (unsigned a = 0; a < nbArea; ++a)
     {
-      IloAnd and_qualitySelection(env);
-      for (unsigned a = 0; a < nbArea; ++a)
+      // 1.ca
+      andSec.add(q_b_qer[u][a] <= q_qer[u][a] * M);
+      // 1.cb
+      andSec.add(q_b_out[u][a] <= (1 - q_qer[u][a]) * M);
+      IloExpr sc_qer(env);
+      IloExpr sc_out(env);
+      for (unsigned j = 0; j < m_conf->nbQer; ++j)
       {
-        and_qualitySelection.add(q_qer[u][a] == created_r[j][a]);
+        // 1.cd
+        andSec.add(q_b_qer[u][a] <= s[u][j] * M + b_qer_j[j]);
+        // 1.ce
+        andSec.add(q_b_out[u][a] <= s[u][j] * M + b_out_j[j]);
       }
-      and_qualitySelection.add(b_qer_j[j] == b_qer[u]);
-      and_qualitySelection.add(b_out_j[j] == b_out[u]);
-      or_repr.add(and_qualitySelection);
+
+      for (unsigned j = 0; j < m_conf->nbQer; ++j)
+      {
+        // 1.b
+        andSec.add( s[u][j] - 1 <= q_qer[u][a] - created_r[j][a] <= 1 - s[u][j] );
+      }
     }
-    or_repr.setName((std::string("1f")).c_str());
-    model.add(or_repr);
+    IloConstraint ic = andSec;
+    ic.setName((std::string("1.c_1.b[")+std::to_string(u)+std::string("]")).c_str());
+    model.add(andSec);
+    // 1.a
+    model.add(IloSum(s[u]) == 1);
+  }
+
+  for (unsigned j = 0; j < m_conf->nbQer; ++j)
+  {
+    //1.d
+    model.add(IloSum(nb[j]) == 1);
+    IloExpr sc_qer(env);
+    IloExpr sc_out(env);
+    for(unsigned n = 0; n <= nbArea; ++n)
+    {
+      sc_qer += nb[j][n] * b_qer_val[n];
+      sc_out += nb[j][n] * b_out_val[n];
+    }
+    //1.e
+    model.add(b_qer_j[j] == sc_qer);
+    //1.f
+    model.add(b_out_j[j] == sc_out);
   }
 
   std::cout << "Generate objective" << std::endl;
@@ -292,7 +328,8 @@ void Optimal::Run(void)
     PrintProgresionBar( float(u)/nbUser,100, std::chrono::microseconds(0), std::chrono::microseconds(0), nbUser-u);
     for (unsigned a = 0; a < nbArea; ++a)
     {
-      sc += v[u][a] * (q_qer[u][a] * b_qer[u] + (1-q_qer[u][a]) * b_out[u]);
+      // sc += v[u][a] * (q_qer[u][a] * b_qer[u] + (1-q_qer[u][a]) * b_out[u]);
+      sc += v[u][a] * (q_b_qer[u][a] + q_b_out[u][a]);
       sc2 += v[u][a];
     }
   }
@@ -303,7 +340,7 @@ void Optimal::Run(void)
     std::cout << std::endl << "Ready to run" << std::endl;
 
     IloCplex cplex(model);
-    cplex.setParam(IloCplex::Threads, 3);
+    cplex.setParam(IloCplex::Threads, 1);
     // cplex.setParam(IloCplex::NodeFileInd,3);
     // cplex.setParam(IloCplex::WorkDir,".");
     // cplex.setParam(IloCplex::MemoryEmphasis,true);
@@ -311,7 +348,6 @@ void Optimal::Run(void)
 
     cplex.exportModel("model.lp");
     cplex.exportModel("model.mps");
-    cplex.exportModel("model.nl");
 
     std::cout << "Start solving" << std::endl;
 
